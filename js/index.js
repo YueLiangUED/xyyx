7422e6448c8a1b907766c617884e15946c0ef607
wangbiaozy <56565283@qq.com>
2017-05-07 14:46:02 +0800

    22

--- js/index.js
+++ js/index.js
@@ -406,5 +406,87 @@ $(function () {
     });
 
 
+    //判断屏幕滚动
+    (function(){
+        var special = jQuery.event.special,
+            uid1 = 'D' + (+new Date()),
+            uid2 = 'D' + (+new Date() + 1);
+        special.scrollstart = {
+            setup: function() {
 
+                var timer,
+                    handler =  function(evt) {
+
+                        var _self = this,
+                            _args = arguments;
+
+                        if (timer) {
+                            clearTimeout(timer);
+                        } else {
+                            evt.type = 'scrollstart';
+                            jQuery.event.dispatch.apply(_self, _args);
+                        }
+
+                        timer = setTimeout( function(){
+                            timer = null;
+                        }, special.scrollstop.latency);
+
+                    };
+
+                jQuery(this).bind('scroll', handler).data(uid1, handler);
+
+            },
+            teardown: function(){
+                jQuery(this).unbind( 'scroll', jQuery(this).data(uid1) );
+            }
+        };
+
+        special.scrollstop = {
+            latency: 300,
+            setup: function() {
+
+                var timer,
+                    handler = function(evt) {
+
+                        var _self = this,
+                            _args = arguments;
+
+                        if (timer) {
+                            clearTimeout(timer);
+                        }
+
+                        timer = setTimeout( function(){
+
+                            timer = null;
+                            evt.type = 'scrollstop';
+                            jQuery.event.dispatch.apply(_self, _args);
+
+                        }, special.scrollstop.latency);
+
+                    };
+
+                jQuery(this).bind('scroll', handler).data(uid2, handler);
+
+            },
+            teardown: function() {
+                jQuery(this).unbind( 'scroll', jQuery(this).data(uid2) );
+            }
+        };
+
+    })();
+
+    (function(){
+        //$('.fixedBtn').css({'opacity':'1'})
+        //滑动屏幕时"我的备选"按钮改变透明度并右移
+        jQuery(window).bind('scrollstart', function(){
+            console.log("start");
+            $('.fixedBtn').animate({'opacity':'0.3','right':'-5rem'})
+        });
+
+        jQuery(window).bind('scrollstop', function(e){
+            console.log("end");
+            $('.fixedBtn').animate({'opacity':'1','right':'2rem'},500)
+        });
+
+    })();
 });
